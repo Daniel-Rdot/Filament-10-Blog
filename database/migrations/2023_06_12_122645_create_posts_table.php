f207eea8e886e680203fd46ea52866573c69bcd4
DanielReitz <d@r.de>
2023-06-13 09:45:26 +0200

    005 - Filament Resources and Layout
    -created CategoryResource with Filament
    -created PostResource with Filament
    -set up and adjusted Filament Forms for these resources

--- database/migrations/2023_06_12_122645_create_posts_table.php
+++ database/migrations/2023_06_12_122645_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,7 +17,7 @@ return new class extends Migration
             $table->string('thumbnail', 2048)->nullable();
             $table->longText('body');
             $table->boolean('active');
-            $table->dateTime('published_at');
+            $table->dateTime('published_at')->nullable();
             $table->foreignIdFor(\App\Models\User::class);
             $table->timestamps();
         });
